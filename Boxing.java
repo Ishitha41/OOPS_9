5af6a2f42d49d0fa92e0de24179e666fa03d87ab
Ishitha41 <2420030041@klh.edu.in>
2025-08-03 19:46:30 +0530

    Add files via upload

---
+++ Boxing.java
@@ -0,0 +1,15 @@
+package myproject;
+
+ 
+
+	public class box {
+	    public static void main(String[] args) {
+	        int num = 20;
+
+	        
+	        Integer obj = Integer.valueOf(num);
+
+	        System.out.println("Primitive value: " + num);
+	        System.out.println("Boxed (Wrapper) value: " + obj);
+	    }
+	}
