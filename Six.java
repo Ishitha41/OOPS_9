b3185a5738bcb20708ba4f5e16e793de3ba9e6c5
Ishitha41 <2420030041@klh.edu.in>
2025-08-09 08:18:40 +0530

    Update Six.java

--- Six.java
+++ Six.java
@@ -7,20 +7,6 @@ public class Six {
 	        int n = sc.nextInt();
 	        int[] arr = new int[n];
 
-	        System.out.println("Enter elements:");
-	        for(int i = 0; i < n; i++) {
-	            arr[i] = sc.nextInt();
-	        }
-
-	        LinkedHashSet<Integer> unique = new LinkedHashSet<>();
-	        for(int num : arr) {
-	            unique.add(num);
-	        }
-
-	        System.out.println("Array after removing duplicates:");
-	        for(int num : unique) {
-	            System.out.print(num + " ");
-	        }
-	    }
+	      
 
 }
